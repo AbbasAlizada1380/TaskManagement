899db0db9ad63e7d40ddc9fc5137b086f5ef5f76
unknown <abbasalizada252@gmail.com>
2025-05-02 10:48:08 +0430

    front is placed correctly

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
+import "./App.css";
+import About from "./Pages/About";
+import Bill from "./Components/Bill_Page/Bill";
+import Services from "./Pages/Services";
+import Dashboard from "./Components/dashboard/dashboard";
+import Loginpage from "./Components/loginpage/loginpage";
+import NotFound from "./Pages/NotFound";
+import ContactUs from "./Pages/ContactUs";
+import BlogCardDetails from "./Components/Blog/BlogCardDetails";
+import ForgotPassword from "./Components/loginpage/ForgotPassword";
+import CreateNewPassword from "./Components/loginpage/CreateNewPassword";
+import CategoryPage from "./Components/Blog/CategoryPage";
+import ServiceDetails from "./Components/Services/ServiceDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogCardDetails />} />
+          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          {/* New route */}
+          <Route path="/service" element={<Services />} />
+          <Route path="/services/:id" element={<ServiceDetails />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+        <Route path="/bill" element={<Bill />} />
+        <Route path="/dashboard" element={<Dashboard role="Designer" />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/forgot-password/" element={<ForgotPassword />} />
+        <Route path="/create-new-password/" element={<CreateNewPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
